f52d92b00ed5e848f2844b9fc3af891b59d703db
petekrisz <69242703+petekrisz@users.noreply.github.com>
2024-06-03 23:36:05 +0200

    Hiányzások feladat
    infojegyzetről

---
+++ Hianyzasok/Absence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hianyzasok
+{
+    public class Absence
+    {
+
+        public string Name;
+        public string Class;
+        public int FirstDay;
+        public int LastDay;
+        public int MissedClasses;
+
+        public Absence (string line)
+        {
+            var temp = line.Split(';');
+            this.Name = temp[0];
+            this.Class = temp[1];
+            this.FirstDay = int.Parse(temp[2]);
+            this.LastDay = int.Parse(temp[3]);
+            this.MissedClasses = int.Parse(temp[4]);
+        }
+        public static IEnumerable<Absence>LoadFromCsv(string filename, Encoding e)
+        {
+            foreach (var item in File.ReadAllLines(filename, e).ToList().Skip(1)) {yield return new Absence(item); }
+        }
+
+    }
+}
---
+++ Hianyzasok/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hianyzasok
+{
+    internal class Program
+    {
+        static List<Absence> data = Absence.LoadFromCsv("szeptember.csv", Encoding.Default).ToList();
+        static void Main(string[] args)
+        {
+            //2. feladat
+            Console.WriteLine($"2. feladat\n\tÖsszes mulasztott órák száma: {data.Sum(x=>x.MissedClasses)} óra.");
+
+            //3. feladat
+            Console.Write($"3. feladat\n\tKérem adjon meg egy napot: ");
+            int day = int.Parse( Console.ReadLine() );
+            Console.Write($"\tTanuló neve: ");
+            string student = Console.ReadLine();
+
+            //4. feladat
+            if (data.Exists(x => x.Name == student)) Console.WriteLine($"4. feladat\n\tA tanuló hiányzott szeptemberben");
+            else Console.WriteLine($"4. feladat\n\tA tanuló nem hiányzott szeptemberben");
+
+            //5. feladat
+            Console.WriteLine($"5. feladat: Hiányzók 2017.09.{day}-n:");
+            bool isThere = false;
+            foreach (var item in data)
+            {
+                if (day >= item.FirstDay && day <= item.LastDay) 
+                { 
+                    Console.WriteLine($"\t{item.Name} ({item.Class})");
+                    isThere = true;
+                }             
+            }
+            if (!isThere) Console.WriteLine("\tNem volt hiányzó");
+
+            //6. feladat
+            var statisztika = data.OrderBy(x => x.Class).GroupBy(x=>x.Class).Select(n => new { Class = n.Key, misses = n.Sum(y=>y.MissedClasses)});
+
+            StreamWriter sw = new StreamWriter("osszesites.csv", false, Encoding.UTF8);
+            foreach (var item in statisztika) sw.WriteLine($"{item.Class};{item.misses}");
+            sw.Close();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
