a6178509ff7e4c5d27bc78f0a3869a17afcff327
petekrisz <69242703+petekrisz@users.noreply.github.com>
2024-06-03 23:39:57 +0200

    ACB kosárlabdaliga feladat
    https://infojegyzet.hu/vizsgafeladatok/okj-programozas/szoftverfejleszto-200526/

---
+++ kosar2004/ACB Liga.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace kosar2004
+{
+    public class ACB_Liga
+    {
+        public string hazai;
+        public string idegen;
+        public int hazai_pont;
+        public int idegen_pont;
+        public string helyszin;
+        public string idopont;
+
+        public ACB_Liga(string line)
+        {
+            string[] hashed = line.Split(';');
+            this.hazai = hashed[0];
+            this.idegen = hashed[1];
+            this.hazai_pont = Convert.ToInt32(hashed[2]);
+            this.idegen_pont = Convert.ToInt32(hashed[3]);
+            this.helyszin = hashed[4];
+            this.idopont = hashed[5];
+        }
+
+        public static IEnumerable<ACB_Liga>LoadFromCsv(string fileName, Encoding e)
+        {
+            foreach (var item in File.ReadAllLines(fileName,e).ToList().Skip(1))
+            {
+                yield return new ACB_Liga(item);
+            }
+        }
+
+    }
+}
---
+++ kosar2004/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kosar2004
+{
+    internal class Program
+    {
+        static List<ACB_Liga> data = ACB_Liga.LoadFromCsv("eredmenyek.csv", Encoding.Default).ToList();
+
+        static void Main(string[] args)
+        {
+            //3. feladat
+
+            // with method realmadrid();
+
+            int hdb = 0, idb = 0;
+            foreach (var item in data)
+            {
+                if(item.hazai == "Real Madrid")
+                {
+                    hdb++;
+                }
+                else if (item.idegen == "Real Madrid")
+                {
+                    idb++;
+                }
+            }
+
+            Console.WriteLine($"3. feladat: Real Madrid: Hazai: {hdb} Idegen: {idb}");
+
+            //4. feladat
+
+
+            bool draw=false;    
+            foreach (var item in data)
+            {
+                if (item.hazai_pont == item.idegen_pont)
+                {
+                    draw=true;
+                    break;
+                }
+            }
+
+            if (draw) { Console.WriteLine($"4. feladat: Volt döntetlen? igen"); }
+            else { Console.WriteLine($"4. feladat: Volt döntetlen? nem"); }
+
+            /*fügvénnyel
+            
+            string valasz = voltdontetlen() ? "4. feladat: Volt döntetlen? igen" : "4. feladat: Volt döntetlen? nem";
+            Console.WriteLine(valasz);*/
+
+            //5. feladat
+            string Barca = "";
+            foreach (var item in data)
+            {
+                if (item.hazai.Contains("Barcelona"))
+                {
+                    Barca= item.hazai;
+                    break;
+                }
+            }
+            Console.WriteLine($"5. feladat: barcelonai csapat neve: {Barca}");
+
+            /* függvénnyel
+
+            Console.WriteLine($"5. feladat: barcelonai csapat neve: {barcaTeljesNeve()}"); ;*/
+
+            //6. feladat
+
+            Console.WriteLine("6. feladat");
+
+         
+            foreach (var item in data)
+            {
+                if(item.idopont == "2004-11-21")
+                {
+                    Console.WriteLine($"\t{item.hazai} - {item.idegen} ({item.hazai_pont}:{item.idegen_pont})");
+                }
+            }
+
+            //7. feladat
+
+
+            Console.WriteLine("7. feladat");
+            statistic();
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+        /*private static string barcaTeljesNeve()
+        {
+            string nev = "";
+            foreach (var item in data)
+            {
+                if (item.hazai.Contains("Barcelona"))
+                {
+                    nev = item.hazai;
+                    break;
+                }
+            }
+            return nev;
+        }*/
+
+        /*private static bool voltdontetlen()
+        {
+            bool dontetlen = false;
+            foreach (var item in data)
+            {
+                if (item.hazai_pont == item.idegen_pont)
+                {
+                    dontetlen = true;
+                    
+                }
+            }
+            return dontetlen;
+        }*/
+
+        private static void statistic()
+        {
+            Dictionary<string, int> stadion = new Dictionary<string, int>();
+            foreach (var item in data)
+            {
+                if (!stadion.ContainsKey(item.helyszin))
+                {
+                    stadion.Add(item.helyszin, 1);
+                }
+                else
+                {
+                    stadion[item.helyszin]++;
+                }
+            }
+            foreach (var item in stadion)
+            {
+                if (item.Value > 20)
+                {
+                    Console.WriteLine($"\t{item.Key}: {item.Value}");
+                }               
+            }
+        }
+
+        /*private static void realmadrid()
+        {
+            int hdb = 0, idb = 0;
+            foreach (var item in data)
+            {
+                if (item.hazai == "Real Madrid")
+                {
+                    hdb++;
+                }
+                else if (item.idegen == "Real Madrid")
+                {
+                    idb++;
+                }
+            }
+
+            Console.WriteLine($"3. feladat: Real Madrid: Hazai: {hdb} Idegen: {idb}");
+        }*/
+
+
+
+    }
+}
+
