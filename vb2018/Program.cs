a7d1f5351742fbaad505f6f57a443688dd74513a
petekrisz <69242703+petekrisz@users.noreply.github.com>
2024-06-03 23:45:06 +0200

    Labdarúgó VB 2018. feladat
    https://infojegyzet.hu/vizsgafeladatok/okj-programozas/rendszeruzemelteto-181002/

---
+++ vb2018/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vb2018
+{
+    public class Program
+    {
+        static List<VB2018> data = VB2018.LoadFromTxt("vb2018.txt", Encoding.Default).ToList();
+        static void Main(string[] args)
+        {
+            //3. fealadat
+            Console.WriteLine($"3. feladat: Stadionok száma: {data.Count}");
+
+            //4. feladat
+            var smallest = data.OrderBy(x => x.Capacity).First();
+            Console.WriteLine($"4. feladat: A legkevesebb férőhely:\n\tVáros: {smallest.City}\n\tStadion neve: {smallest.Stadion}\n\tFérőhely: {smallest.Capacity}");
+
+            //5. feladat
+            Console.WriteLine($"5. feladat: Átlagos férőhelyszám: {data.Average(x=>x.Capacity): .#}");
+
+            //6. feladat
+            Console.WriteLine($"6. feladat: A két néven is ismert stadionok száma: {data.Where(x => x.Alias != "n.a.").Count()}");
+
+            //7. feladat
+            string cityQuery = "";
+            do
+            {
+                Console.Write("7. feladat: Kérem a város nevét: ");
+                cityQuery = Console.ReadLine().ToLower();
+            } while (cityQuery.Length < 3);
+
+            //8. feladat
+            if (data.Exists(x => x.City.ToLower() == cityQuery)) Console.WriteLine("8. feladat: A megadott város VB helyszín.");
+            else Console.WriteLine("8. feladat: A megadott város nem VB helyszín.");
+
+            //9. feladat
+            Console.WriteLine($"9. feladat: {data.GroupBy(x => x.City).Count()} különböző városban voltak mérkőzések.");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ vb2018/VB2018.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace vb2018
+{
+    internal class VB2018
+    {
+        public string City;
+        public string Stadion;
+        public string Alias;
+        public int Capacity;
+        public VB2018(string line)
+        {
+            var temp = line.Split(';');
+            this.City = temp[0];
+            this.Stadion = temp[1];
+            this.Alias = temp[2];
+            this.Capacity = int.Parse(temp[3]);
+        }
+
+        public static IEnumerable<VB2018>LoadFromTxt(string filename, Encoding e)
+        {
+            foreach (var item in File.ReadAllLines(filename, e).ToList().Skip(1))
+            {
+                yield return new VB2018(item);
+            }
+            
+        }
+    }
+}
