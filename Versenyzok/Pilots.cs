8a23bcf2954df59d4b1124b6f43b648118379643
petekrisz <69242703+petekrisz@users.noreply.github.com>
2024-06-03 23:46:06 +0200

    Pilóták feladat
    https://infojegyzet.hu/vizsgafeladatok/okj-programozas/szoftverfejleszto-210209/

---
+++ Versenyzok/Pilots.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+
+namespace Versenyzok
+{
+    public class Pilots
+    {
+        public string Name;
+        public DateTime BirthDate;
+        public string Nationality;
+        public int Number;
+
+        public Pilots(string line) 
+        { 
+            if (line.EndsWith(";"))
+            {
+                line.Substring(0, line.Length - 1);
+                var temp = line.Split(';');
+                this.Name = temp[0];
+                this.BirthDate = DateTime.ParseExact(temp[1], "yyyy.MM.dd", CultureInfo.InvariantCulture);
+                this.Nationality = temp[2];
+            }
+            else 
+            {
+                var temp = line.Split(';');
+                this.Name = temp[0];
+                this.BirthDate = DateTime.ParseExact(temp[1], "yyyy.MM.dd", CultureInfo.InvariantCulture);
+                this.Nationality = temp[2];
+                this.Number = int.Parse(temp[3]);
+            }
+        }
+
+        public static IEnumerable<Pilots>LoadFromCsv(string fileName,Encoding e)
+        {
+            foreach (var item in File.ReadAllLines(fileName,e).ToList().Skip(1))
+            {
+                yield return new Pilots(item);
+            }
+        }
+
+
+    }
+}
---
+++ Versenyzok/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Versenyzok
+{
+   
+    internal class Program
+    {
+        static List<Pilots> data = Pilots.LoadFromCsv("pilotak.csv", Encoding.UTF8).ToList();
+
+        static void Main(string[] args)
+        {  
+            Console.WriteLine($"3. feladat: {data.Count}");
+            Console.WriteLine($"4. feladat: {data.Select(x => x.Name).Last()}");
+            Console.WriteLine("5. feladat:");
+            DateTime szford = new DateTime(1901,1,1);
+            var multsz = data.Where(x => x.BirthDate < szford);
+            foreach ( var item in multsz ) { Console.WriteLine($"\t{item.Name} ({item.BirthDate:yyyy. MM. dd.})"); }
+            Console.WriteLine($"6. feladat: {data.Where(r=>r.Number >0).OrderBy(x => x.Number).Select(y=>y.Nationality).First()}");
+            var statisztika = data.GroupBy(x => x.Number).Select(n => new { Number = n.Key, db = n.Count() });
+            Console.Write($"7. feladat: ");
+            List<int> polenumbers = new List<int>();
+            foreach ( var item in statisztika) 
+            { 
+                if (item.db > 1 && item.Number !=0) polenumbers.Add(item.Number);
+            }
+            for ( int i = 0; i < polenumbers.Count-1; i++ )
+            {
+                Console.Write($"{polenumbers[i]}, ");
+            }
+            Console.Write($"{polenumbers[polenumbers.Count-1]}");
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
