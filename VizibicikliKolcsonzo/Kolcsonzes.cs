3f60c7a6d9b24a60c7272278b7ef0c5ff0090d85
petekrisz <69242703+petekrisz@users.noreply.github.com>
2024-06-03 23:47:00 +0200

    Vizibicikli Kölcsönző feladat
    https://infojegyzet.hu/vizsgafeladatok/okj-programozas/szoftverfejleszto-220208/

---
+++ VizibicikliKolcsonzo/Kolcsonzes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VizibicikliKolcsonzo
+{
+    internal class Kolcsonzes
+    {
+        public string Nev;
+        public string JAzon;
+        public int EOra;
+        public int EPerc;
+        public int VOra;
+        public int VPerc;
+
+        public Kolcsonzes(string line)
+        {
+            var temp = line.Split(';');
+            Nev = temp[0];
+            JAzon = temp[1];
+            EOra = int.Parse(temp[2]);
+            EPerc = int.Parse(temp[3]);
+            VOra = int.Parse(temp[4]);
+            VPerc = int.Parse(temp[5]);
+
+        }
+
+        public int Epercben
+        {
+            get 
+            { 
+                return EOra*60+EPerc;
+            }
+        }
+        public int Vpercben
+        {
+            get
+            {
+                return VOra * 60 + VPerc;
+            }
+        }
+
+        public static IEnumerable<Kolcsonzes> LoadFromTxt(string filename, Encoding e)
+        {
+            foreach (var item in File.ReadAllLines(filename, e).ToList().Skip(1))
+            {
+                yield return new Kolcsonzes(item);
+            }
+        }
+    }
+}
---
+++ VizibicikliKolcsonzo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VizibicikliKolcsonzo
+{
+    internal class Program
+    {
+        static List<Kolcsonzes> data = Kolcsonzes.LoadFromTxt("kolcsonzesek.txt", Encoding.UTF8).ToList();
+
+        static void Main(string[] args)
+        {
+
+            //5. feladat
+            Console.WriteLine($"5. feladat: Napi kölcsönzesek száma: {data.Count}");
+
+            //6. feladat
+            Console.Write($"6. feladat: Kérek egy nevet: ");
+            string queryName = Console.ReadLine();
+            if (data.Exists(x => x.Nev == queryName))
+            {
+                Console.WriteLine($"\t{queryName} kölcsönzései:");
+                foreach (Kolcsonzes item in data)
+                {
+                    if (item.Nev == queryName) Console.WriteLine($"\t{item.EOra:00}:{item.EPerc:00} - {item.VOra:00}:{item.VPerc:00}");
+                }
+            }
+            else Console.WriteLine($"\t Nem volt ilyen nevű kölcsönző");
+
+            //7. feladat
+
+            Console.Write("7. feladat: Adjon meg egy időpontot óra:perc alakban: ");
+            var time = Console.ReadLine().Split(':');
+            int percben = int.Parse(time[0]) *60 + int.Parse(time[1]);
+            Console.WriteLine($"\tA vízen lévő járművek {int.Parse(time[0])}:{int.Parse(time[1]):00}-kor:");
+            foreach (var item in data)
+            {
+                if ( percben >= item.Epercben && percben <= item.Vpercben)
+                {
+                    Console.WriteLine(  $"\t{item.EOra:00}:{item.EPerc:00}-{item.VOra:00}:{item.VPerc:00} : {item.Nev}");
+                }
+            }
+
+            //8. feladat
+            int bevetel = 0;
+            foreach (var item in data) 
+            {
+                int kezdet = item.Epercben;
+                while (kezdet < item.Vpercben) 
+                { 
+                    bevetel +=2400;
+                    kezdet += 30;
+                }
+            }
+
+            Console.WriteLine($"8. feladat: A napi bevétel: {bevetel} Ft");
+
+            //9. feladat
+            StreamWriter sw = new StreamWriter("F.txt", false, Encoding.UTF8);
+            var Fsuspect = data.Where(x => x.JAzon == "F");
+            foreach (var item in Fsuspect) { sw.WriteLine($"{item.EOra:00}:{item.EPerc:00}-{item.VOra:00}:{item.VPerc:00} : {item.Nev}" ); }           
+            sw.Close();
+
+            //10.feladat
+
+            var statisztika = data.GroupBy(x => x.JAzon).Select(n => new { JAzon = n.Key, db = n.Count() }).OrderBy(x => x.JAzon);
+            Console.WriteLine("10. feladat: Statisztika: ");
+            foreach (var item in statisztika) { Console.WriteLine($"\t{item.JAzon} - {item.db}"); }
+
+            Console.ReadKey();
+        }
+    }
+}
