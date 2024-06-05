6a29be811bd92760d5d83a0db50505d2a2e61601
petekrisz <69242703+petekrisz@users.noreply.github.com>
2024-06-05 13:55:49 +0200

    Műkorcsolya feladat
    http://infojegyzet.hu/vizsgafeladatok/okj-programozas/szoftverfejleszto-180517/

---
+++ Helsink2017/Korcsolya.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using System.Globalization;
+
+namespace Helsink2017
+{
+    public class Korcsolya
+    {
+        public string Name;
+        public string Country;
+        public double Technical;
+        public double Component;
+        public int Minus;
+
+
+        public Korcsolya(string line)
+        { 
+            var temp = line.Split(';');
+            Name = temp[0];
+            Country = temp[1];
+            Technical = double.Parse(temp[2], CultureInfo.InvariantCulture);
+            Component = double.Parse(temp[3], CultureInfo.InvariantCulture);
+            Minus = int.Parse(temp[4]);
+        }
+
+        public static IEnumerable<Korcsolya> LoadFromCsv(string fileName, Encoding e) 
+        { 
+            foreach (var item in File.ReadAllLines(fileName,e).ToList().Skip(1)) yield return new Korcsolya(item);
+        }
+
+    }
+}
---
+++ Helsink2017/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.CodeDom;
+
+namespace Helsink2017
+{
+    public class Program
+    {
+        static List<Korcsolya> shortProgram = Korcsolya.LoadFromCsv("rovidprogram.csv", Encoding.UTF8).ToList();
+        static List<Korcsolya> final = Korcsolya.LoadFromCsv("donto.csv", Encoding.UTF8).ToList();
+
+        static void Main(string[] args)
+        {
+
+
+
+            //2. feladat
+            Console.WriteLine($"2. feladat\n\tA rövidprogramban {shortProgram.Count} induló volt.");
+
+            //3. feladat
+            if (final.Exists(x=>x.Country == "HUN")) Console.WriteLine("3. feladat\n\tA magyar versenyző bejutott a kűrbe.");
+            else Console.WriteLine("3. feladat\n\tA magyar versenyző nem jutott be a kűrbe.");
+
+            //5. feladat
+            Console.Write("5. feladat\n\tKérem a versenyző nevét: "  );
+            string nev = Console.ReadLine();
+            //6. feladat
+            if (shortProgram.Exists(x=>x.Name == nev)) Console.WriteLine($"6. feladat\n\tA versenyző összpontszáma: {TotalPoints(nev)}");
+            else Console.WriteLine("\tIlyen nevű induló nem volt!");
+
+            //7. feladat
+            var statistic = final.GroupBy(x => x.Country).Select(n => new { country = n.Key, db = n.Count() });
+            Console.WriteLine("7. feladat");
+            foreach (var item in statistic.Where(x=>x.db>1))
+            {
+                Console.WriteLine($"\t{item.country}: {item.db} versenyző");
+            }
+
+            //8. feladat
+            int rank = 1;
+            StreamWriter sw=new StreamWriter("vegeredmeny.csv", false, Encoding.UTF8);
+            foreach(var item in shortProgram.OrderByDescending(x => TotalPoints(x.Name))) 
+            {
+                sw.WriteLine($"{rank}; {item.Name}; {item.Country}; {TotalPoints(item.Name)}");
+                rank++;
+            }
+            sw.Close();
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+        private static double TotalPoints(string name)
+        {
+            double total = 0;
+            Korcsolya person = shortProgram.Where(x => x.Name == name).FirstOrDefault();
+            if (final.Exists(x => x.Name == person.Name)) total += final.Where(x => x.Name == person.Name).Select(x => x.Technical+x.Component-x.Minus).Sum();
+            total += person.Technical + person.Component - person.Minus;
+            return total;
+
+        }
+    }
+}
