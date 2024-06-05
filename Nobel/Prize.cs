cbb64ad3feb4f8247170eca653118f84c0efb123
petekrisz <69242703+petekrisz@users.noreply.github.com>
2024-06-05 13:57:07 +0200

    Nobel-díjak feladat
    http://infojegyzet.hu/vizsgafeladatok/okj-programozas/szoftverfejleszto-181002/

---
+++ Nobel/Prize.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Nobel
+{
+    public class Prize
+    {
+        public int Year;
+        public string Type;
+        public string FirstName;
+        public string LastName;
+
+        public Prize (string line)
+        {
+            var temp = line.Split(';');
+            Year = int.Parse(temp[0]);
+            Type = temp[1];
+            FirstName = temp[2];
+            LastName = temp[3];
+        }
+
+        public static IEnumerable<Prize>LoadFromCsv(string fileName, Encoding e)
+        {
+            foreach (var item in File.ReadAllLines(fileName, e).ToList().Skip(1)) 
+            { 
+                yield return new Prize (item);
+            }
+        }
+
+    }
+}
---
+++ Nobel/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobel
+{
+    public class Program
+    {
+        static List<Prize> data=Prize.LoadFromCsv("nobel.csv", Encoding.Default).ToList();
+        static void Main(string[] args)
+        {
+            //3. feladat
+            Console.WriteLine($"3. feladat: {data.Where(x=>x.LastName == "McDonald" && x.FirstName == "Arthur B.").Select(x=>x.Type).FirstOrDefault()}");
+
+            //4. feladat
+            var lit2017 = data.Where(x => x.Year == 2017 && x.Type == "irodalmi").ToList();                      
+            Console.Write($"4. feladat: ");
+            foreach( var x in lit2017 ) {  Console.Write($"{x.FirstName} {x.LastName}\t "); }
+
+                //Ha feltételezem, hogy csak 1 van:
+                Console.WriteLine($"\n4. feladat: {data.Where(x => x.Year == 2017 && x.Type == "irodalmi").Select(x=>x.FirstName).FirstOrDefault()} {data.Where(x => x.Year == 2017 && x.Type == "irodalmi").Select(x => x.LastName).FirstOrDefault()}");
+
+            //5. feladat
+            var organizations = data.Where(x => x.LastName == "" && x.Year>=1990).ToList();
+            Console.WriteLine($"5. feladat: ");
+            foreach (var x in organizations) { Console.WriteLine($"\t{x.Year}: {x.FirstName}"); }
+
+            //6. feladat
+            var Curie = data.Where(x => x.LastName.Contains("Curie")).ToList();
+            Console.WriteLine($"6. feladat: ");
+            foreach (var x in Curie) { Console.WriteLine($"\t{x.Year}: {x.FirstName} {x.LastName} ({x.Type})"); }
+
+            //7. feladat
+
+            var statistics = data.GroupBy(x => x.Type).Select(n => new { Type = n.Key, db = n.Count() });
+            Console.WriteLine("7. feladat:");
+            foreach(var item in statistics) Console.WriteLine($"\t{item.Type,-30}{item.db,3} db");
+
+            //8. feladat
+            Console.WriteLine("8. feladat: orvosi.txt");
+            StreamWriter sw = new StreamWriter("orvosi.txt", false, Encoding.UTF8);
+            var med = data.Where(x => x.Type == "orvosi").OrderBy(x=>x.Year).ToList();
+            foreach (var item in med) { sw.WriteLine($"{item.Year}:{item.FirstName} {item.LastName}"); }
+            sw.Close();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
